8a76b38b22d1b9e2d41f8368ffa3d7b0212f612d
Илья Кротких <ivashca99@gmail.com>
2018-02-21 20:44:40 +0300

    Добавьте файлы проекта.

---
+++ DiscreteMaC_Lib/Edge.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscreteMaC_Lib
+{
+    public class Edge
+    {
+        public string Name;
+        public Point StartPoint { get; set; }
+        public Point EndPoint { get; set; }
+
+        public Edge(string Name, Point StartPoint, Point EndPoint)
+        {
+            this.Name = Name;
+            this.StartPoint = StartPoint;
+            this.EndPoint = EndPoint;
+        }
+    }
+}
---
+++ DiscreteMaC_Lib/GraphNotations/TypedGraphNotation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscreteMaC_Lib.GraphNotations
+{
+    public abstract class TypedGraphNotation<CustomType>: GraphNotation
+    {
+        public abstract CustomType ConvertFromGrapch(Graph InitialGraph) ;
+        public abstract Graph ConvertToGrapch(CustomType InitialGraph);
+    }
+}
