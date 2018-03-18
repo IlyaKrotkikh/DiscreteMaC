7d7873c36caf42caee19bc8f78aa701fcecb4ac1
Илья Кротких <ivashca99@gmail.com>
2018-03-19 01:46:45 +0300

    Commented compare by name point.

--- DiscreteMaC_Lib/Graphes/Edges/EdgeComparers/PointEdgeEqualityComparer.cs
+++ DiscreteMaC_Lib/Graphes/Edges/EdgeComparers/PointEdgeEqualityComparer.cs
@@ -24,8 +24,8 @@ namespace DiscreteMaC_Lib.Graphes.Edges.EdgeComparers
                 return true;
             else if (edge1 == null | edge2 == null)
                 return false;
-            else if (edge1.Name == edge2.Name)
-                return true;
+            //else if (edge1.Name == edge2.Name)
+            //    return true;
             else if (edge1.StartPoint == edge2.StartPoint && edge1.EndPoint == edge2.EndPoint)
                 return true;
             else
